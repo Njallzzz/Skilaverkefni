8d06cf6e6a9b9b8602ae8b82ecafe4191901472c
“Njallzzz” <njalli123@gmail.com>
2015-12-10 22:19:16 +0000

    initial push - skilaverkefni3

---
+++ skilaverkefni3/mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    model = new QStandardItemModel(10,4,this);
+
+    model->setHeaderData( 0, Qt::Horizontal, "Name" );
+    model->setHeaderData( 1, Qt::Horizontal, "Gender" );
+    model->setHeaderData( 2, Qt::Horizontal, "Birth" );
+    model->setHeaderData( 3, Qt::Horizontal, "Death" );
+
+    for(int row = 0; row < 10; row++){
+        for(int col = 0; col < 2; col++) {
+            QModelIndex index = model->index( row, col, QModelIndex() );
+            model->setData( index, "hello" );
+        }
+    }
+
+    ui->tableView->setModel(model);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
