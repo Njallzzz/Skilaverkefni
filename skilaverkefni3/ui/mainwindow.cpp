83f8979251db59712f1b3cab7e7d82e8f71dc502
Arnór Brynjólfsson <arnorbry@gmail.com>
2015-12-15 15:33:01 +0000

    fixed crash on addPerson

--- skilaverkefni3/ui/mainwindow.cpp
+++ skilaverkefni3/ui/mainwindow.cpp
@@ -320,6 +320,7 @@ void MainWindow::on_people_list_customContextMenuRequested(const QPoint &pos)
 void MainWindow::on_action_Add_Person_triggered()
 {
     PersonWindow *pWindow = new PersonWindow;
+    pWindow->getHandler( handler );
     pWindow->exec();
     delete pWindow;
     displayPeople();
